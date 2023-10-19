df3f2c0d00cad4acf70d711f2de2f71341f7172b
Daria Nikulina <daryanickulina@yandex.ru>
2023-10-19 21:00:32 +0400

    feat: slider switching

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useEffect } from "react";
 import { VideoContext } from "../../store/store";
 
 export default function Slider({ videos }) {
-  const { videoContext } = useContext(VideoContext);
+  const { videoContext, setVideoContext } = useContext(VideoContext);
   const [leftImage, setLeftImage] = React.useState(null);
   const [rightImage, setRightImage] = React.useState(null);
 
@@ -21,35 +21,58 @@ export default function Slider({ videos }) {
         }
       });
     });
-  }, []);
+  }, [videoContext]);
+
+  function handleSwitchRight() {
+    setVideoContext(rightImage);
+  }
+
+  function handleSwitcLeft() {
+    setVideoContext(leftImage);
+  }
 
   return (
     <section className={styles.sliderBlock}>
-      <div className={styles.photoBlock}>
-        <img
-          src={leftImage?.photo}
-          alt={leftImage?.text}
-          className={styles.image}
-        />
-        <p className={styles.text}>{leftImage?.text}</p>
-      </div>
+      <ul className={styles.sliderBlock}>
+        <li className={styles.photoBlock}>
+          <img
+            src={leftImage?.photo}
+            alt={leftImage?.text}
+            className={styles.image}
+          />
+          <p className={styles.text}>{leftImage?.text}</p>
+        </li>
 
-      <div className={styles.photoBlock}>
-        <img
-          src={videoContext?.photo}
-          alt={videoContext?.text}
-          className={styles.image}
-        />
-        <p className={styles.text}>{videoContext?.text}</p>
-      </div>
+        <li className={styles.photoBlock}>
+          <img
+            src={videoContext?.photo}
+            alt={videoContext?.text}
+            className={styles.image}
+          />
+          <p className={styles.text}>{videoContext?.text}</p>
+        </li>
 
-      <div className={styles.photoBlock}>
-        <img
-          src={rightImage?.photo}
-          alt={rightImage?.text}
-          className={styles.image}
+        <li className={styles.photoBlock}>
+          <img
+            src={rightImage?.photo}
+            alt={rightImage?.text}
+            className={styles.image}
+          />
+          <p className={styles.text}>{rightImage?.text}</p>
+        </li>
+      </ul>
+
+      <div className={styles.swipe}>
+        <button
+          type="button"
+          className={styles.leftSwipe}
+          onClick={handleSwitcLeft}
         />
-        <p className={styles.text}>{rightImage?.text}</p>
+        <button
+          type="button"
+          className={styles.rightSwipe}
+          onClick={handleSwitchRight}
+        />
       </div>
     </section>
   );
